4e94daa8fd8d23bdb02e10b833e5132a47bd6adb
JK Baseer <61427976+jk-baseer@users.noreply.github.com>
2021-12-04 20:48:36 +0530

    mdx-successfully-tested

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
- module.exports = {
-		images: {
-			domains: [
-				"storage.googleapis.com",
-				"tradly-paas-sandbox.s3.amazonaws.com",
-				"tradly-paas.s3.amazonaws.com",
-			],
-		},
-		env: {
-			ENVIRONMENT: process.env.ENVIRONMENT,
-			BASE_URL:process.env.BASE_URL,
-			API_KEY: process.env.API_KEY,
-			SITE_URL: process.env.SITE_URL,
-		},
- };
+const withMDX = require('@next/mdx')({
+	extension: /\.mdx?$/,
+  });
+  module.exports = withMDX({
+	pageExtensions: ['js', 'jsx', 'mdx', 'md'],
+	images: {
+	  domains: [
+		'storage.googleapis.com',
+		'tradly-paas-sandbox.s3.amazonaws.com',
+		'tradly-paas.s3.amazonaws.com',
+	  ],
+	},
+	env: {
+	  ENVIRONMENT: process.env.ENVIRONMENT,
+	  BASE_URL: process.env.BASE_URL,
+	  API_KEY: process.env.API_KEY,
+	  SITE_URL: process.env.SITE_URL,
+	},
+  });
